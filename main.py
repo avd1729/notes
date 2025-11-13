37118c584618c7e0e4ea36bd3f252d604274ba50
Aravind <aravind19112003@gmail.com>
2025-11-13 22:15:03 +0530

    add models and sample endpoint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from core import db
+from models import user
+
+user.Base.metadata.create_all(bind = db.engine)
 
 app = FastAPI()
 
@@ -10,3 +13,16 @@ def get_db():
         yield db_session
     finally:
         db_session.close()
+
+
+@app.post("/users/")
+def create_user(username: str, email: str, db: Session = Depends(get_db)):
+    user_instance = user.User(username=username, email=email)
+    db.add(user_instance)
+
+    db.commit()
+    db.refresh(user_instance)
+    return {
+        "name": username,
+        "email": email
+    }
